4cd83801300ea50a1b0fe29b68afa81425d2be0c
AnhPhi2002 <phinhase161377@fpt.edu.vn>
2024-11-01 15:15:33 +0700

    cau trau lib

--- src/lib/api/redux/counterSlice.ts
+++ src/lib/api/redux/counterSlice.ts
@@ -1,37 +1,37 @@
-// counterSlice.ts
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../store';
+// // counterSlice.ts
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { RootState } from '../store';
 
 
-// Define a type for the slice state
-export interface CounterState {
-  value: number;
-}
+// // Define a type for the slice state
+// export interface CounterState {
+//   value: number;
+// }
 
-// Define the initial state using that type
-const initialState: CounterState = {
-  value: 0,
-};
+// // Define the initial state using that type
+// const initialState: CounterState = {
+//   value: 0,
+// };
 
-export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
-    },
-  },
-});
+// export const counterSlice = createSlice({
+//   name: 'counter',
+//   initialState,
+//   reducers: {
+//     increment: (state) => {
+//       state.value += 1;
+//     },
+//     decrement: (state) => {
+//       state.value -= 1;
+//     },
+//     incrementByAmount: (state, action: PayloadAction<number>) => {
+//       state.value += action.payload;
+//     },
+//   },
+// });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
-// Selector
-export const selectCount = (state: RootState) => state.counter.value;
+// // Selector
+// export const selectCount = (state: RootState) => state.counter.value;
 
-export default counterSlice.reducer;
+// export default counterSlice.reducer;
