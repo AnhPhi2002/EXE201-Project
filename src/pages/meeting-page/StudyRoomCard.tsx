756b61b76df62a78434af79129500552cad0cc6a
Huy <huybqse161436@fpt.edu.vn>
2024-11-22 11:01:05 +0700

    fix bug StudyRoomCard

--- src/pages/meeting-page/StudyRoomCard.tsx
+++ src/pages/meeting-page/StudyRoomCard.tsx
@@ -38,6 +38,7 @@ export const StudyRoomCard: React.FC<StudyRoomCardProps> = ({ post, onJoin }) =>
       <div className="space-y-4">
         <div className="bg-blue-50/50 rounded-lg p-4 backdrop-blur-sm border border-blue-100/30">
           <div className="flex items-center gap-2 text-gray-700 mb-2">
+          <FaClock/>
             <span className="font-medium text-sm">
               {`${post.startTime} - ${post.endTime}, ${post.date}`}
             </span>
