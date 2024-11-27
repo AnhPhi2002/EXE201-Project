666fdaf00cfe5af02505133078a52f626f581c00
Huy <huybqse161436@fpt.edu.vn>
2024-11-28 01:30:33 +0700

    call api comment subject

--- src/lib/api/store.ts
+++ src/lib/api/store.ts
@@ -9,6 +9,7 @@ import { userReducer } from './redux/userSlice';
 import postReducer from './redux/postSlice';
 import roomReducer from './redux/roomSlice'
 import commentReducer from './redux/commentSlice'
+import commentSubjectReducer from './redux/commentSubjectSlice';
 
 
 const rootReducer = combineReducers({
@@ -22,6 +23,7 @@ const rootReducer = combineReducers({
   posts: postReducer,
   rooms: roomReducer,
   comment: commentReducer,
+  commentSubject: commentSubjectReducer,
 });
 
 export const store = configureStore({
