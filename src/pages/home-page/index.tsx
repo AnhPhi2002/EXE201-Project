434148013f99304167eadca450460d5ede2a3f37
AnhPhi2002 <phinhase161377@fpt.edu.vn>
2024-11-28 10:59:53 +0700

    feat: comment home

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -4,6 +4,9 @@ import HeroSection from './HeroSection';
 import PortCard from './PostCard';
 import ResourceCard from './Resource';
 import MeetingCard from './MeetingCard';
+// import CommentHome from './CommentHome';
+
+
 
 const LandingPage: React.FC = () => {
   useEffect(() => {
@@ -22,6 +25,7 @@ const LandingPage: React.FC = () => {
         <ResourceCard />
         {/* Meeting Section */}
         <MeetingCard />
+        {/* <CommentHome /> */}
       </div>
     </div>
   );
